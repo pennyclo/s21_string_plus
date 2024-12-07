367b8bdd3f66bf5b7513b6bbd27e3e20ac2e5584
pennyclo <alexandr_sav@outlook.com>
2024-12-07 19:53:39 +0500

    add: implementation function s21_strlen

---
+++ src/include/s21_strlen.h
@@ -0,0 +1,19 @@
+/**
+ * @file s21_strlen.h
+ * @author Alexandr Afanasev (https://github.com/pennyclo)
+ * @brief Header file with function declaration s21_strlen.
+ * @version 1.0
+ * @date 2024-12-07
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_INCLUDE_S21_STRLEN_H_
+#define SRC_INCLUDE_S21_STRLEN_H_
+
+#include "include/common.h"
+
+s21_size_t s21_strlen(const char *str);
+
+#endif  // SRC_INCLUDE_S21_STRLEN_H_
---
+++ src/modules/s21_strlen.c
@@ -0,0 +1,33 @@
+/**
+ * @file s21_strlen.c
+ * @author Alexandr Afanasev (https://github.com/pennyclo)
+ * @brief Implementation function s21_strlen.
+ * @version 1.0
+ * @date 2024-12-07
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "include/s21_strlen.h"
+
+/**
+ * @brief Calculates the length of a C-style string.
+ *
+ * @param str A pointer to the null-terminated string.
+ * @return s21_size_t (without taking into account the zero terminator).
+ *
+ * @note This function does not handle NULL input. Passing a NULL pointer will
+ * result in undefined behavior. The caller is responsible for ensuring that the
+ * input pointer is valid.
+ */
+s21_size_t s21_strlen(const char *str) {
+  s21_size_t lenght = 0;
+
+  while (*str != '\0') {
+    lenght++;
+    str++;
+  }
+
+  return lenght;
+}

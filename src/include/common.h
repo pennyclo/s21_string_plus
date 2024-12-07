17fcf5c2498c6bf316a6133e9613b77bb03d8783
pennyclo <alexandr_sav@outlook.com>
2024-12-07 19:52:10 +0500

    add: common header & type s21_size_t

---
+++ src/include/common.h
@@ -0,0 +1,17 @@
+/**
+ * @file s21_size_t.h
+ * @author Alexandr Afanasev (https://github.com/pennyclo)
+ * @brief Type definition for s21_size_t.
+ * @version 1.0
+ * @date 2024-12-07
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_INCLUDE_COMMON_H_
+#define SRC_INCLUDE_COMMON_H_
+
+typedef unsigned long s21_size_t;
+
+#endif  // SRC_INCLUDE_COMMON_H_

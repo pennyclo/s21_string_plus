9f63d8fc79e838a912772f935645452b3fe59fef
pennyclo <alexandr_sav@outlook.com>
2024-12-16 17:13:47 +0500

    test(strcpy): add new checks in case

--- src/tests/modules_test/s21_strcpy_test.c
+++ src/tests/modules_test/s21_strcpy_test.c
@@ -17,21 +17,26 @@ START_TEST(s21_strcpy_1) {
   char src[] = "Hello, world!";
   char dest[50];
   char *result = s21_strcpy(dest, src);
-  ck_assert_str_eq(dest, src);
+
+  ck_assert_str_eq(result, dest);
   ck_assert_ptr_eq(result, dest);
+  ck_assert_str_eq(dest, src);
 }
 
 START_TEST(s21_strcpy_2) {
   char src[] = "";
   char dest[50];
   char *result = s21_strcpy(dest, src);
-  ck_assert_str_eq(dest, src);
+
+  ck_assert_str_eq(result, dest);
   ck_assert_ptr_eq(result, dest);
+  ck_assert_str_eq(dest, src);
 }
 
 START_TEST(s21_strcpy_3) {
   char src_dest[50] = "Test";
   char *result = s21_strcpy(src_dest, src_dest);
+
   ck_assert_str_eq(src_dest, "Test");
   ck_assert_ptr_eq(result, src_dest);
 }
@@ -44,55 +49,69 @@ START_TEST(s21_strcpy_4) {
   src[1023] = '\0';
   char dest[1024];
   char *result = s21_strcpy(dest, src);
-  ck_assert_str_eq(src, dest);
+
+  ck_assert_str_eq(result, dest);
   ck_assert_ptr_eq(result, dest);
+  ck_assert_str_eq(dest, src);
 }
 
 START_TEST(s21_strcpy_5) {
   char dest[50];
   char *src = "\n";
   char *result = s21_strcpy(dest, src);
-  ck_assert_str_eq(result, src);
+
+  ck_assert_str_eq(result, dest);
+  ck_assert_ptr_eq(result, dest);
+  ck_assert_str_eq(dest, src);
 }
 
 START_TEST(s21_strcpy_6) {
   char src[] = "Test";
   char dest[] = "test";
   char *result = s21_strcpy(dest, src);
+
   ck_assert_str_eq(result, "Test");
   ck_assert_str_eq(result, src);
 }
 
 START_TEST(s21_strcpy_7) {
-  char src_dest[50] = "This is a test";
-  char *result = s21_strcpy(src_dest + 5, src_dest);  // Overlap
-  ck_assert_str_eq(result, src_dest + 5);  // Check that the function has worked
-  ck_assert_ptr_eq(result, src_dest + 5);  // Should be the same pointer as dest
+  char src[] = "Line1\nLine2\tEnd";
+  char dest[50] = {0};
+  char *result = s21_strcpy(dest, src);
+
+  ck_assert_str_eq(result, dest);
+  ck_assert_str_eq(dest, src);
+  ck_assert_ptr_eq(result, dest);
 }
 
 START_TEST(s21_strcpy_8) {
   char src[] = "This is a string with spaces and punctuation.";
   char dest[100];
   char *result = s21_strcpy(dest, src);
+
+  ck_assert_str_eq(result, dest);
   ck_assert_str_eq(dest, src);
   ck_assert_ptr_eq(result, dest);
 }
 
 START_TEST(s21_strcpy_9) {
   char src[] = "This is a long string";
-  char dest[10];
+  char dest[30];
   char *result = s21_strcpy(dest, src);
-  ck_assert_ptr_eq(result, dest);  // Check the function hasn't crashed
-  // Check for unexpected behavior (e.g., partial copy, buffer overflow)
-  ck_assert_str_ne(dest, src);  // expect only part of the string to be copied.
+
+  ck_assert_str_eq(result, dest);
+  ck_assert_ptr_eq(result, dest);
+  ck_assert_str_eq(dest, src);
 }
 
 START_TEST(s21_strcpy_10) {
   char src[] = "\0";
   char dest[100];
   char *result = s21_strcpy(dest, src);
-  ck_assert_str_eq(dest, src);
+
+  ck_assert_str_eq(result, dest);
   ck_assert_ptr_eq(result, dest);
+  ck_assert_str_eq(dest, src);
 }
 
 Suite *s21_strcpy_case_1(void) {

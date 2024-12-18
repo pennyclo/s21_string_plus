d132f25ce13855edb84e6853a43ad1fef4ade424
pennyclo <alexandr_sav@outlook.com>
2024-12-18 18:17:12 +0500

    feat(strerror): add s21_strerror function

---
+++ src/include/s21_strerror.h
@@ -0,0 +1,19 @@
+/**
+ * @file s21_strerror.h
+ * @author Alexandr Afanasev (https://github.com/pennyclo)
+ * @brief Header file with function declaration s21_strerror.
+ * @version 1.0
+ * @date 2024-12-17
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_INCLUDE_S21_STRERROR_H_
+#define SRC_INCLUDE_S21_STRERROR_H_
+
+#include "include/common.h"
+
+char *s21_strerror(int errnum);
+
+#endif  // SRC_INCLUDE_S21_STRERROR_H_

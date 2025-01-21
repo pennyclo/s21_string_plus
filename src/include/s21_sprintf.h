fc239e79e3a15a2224a0744aab04a14f96c610d8
pennyclo <alexandr_sav@outlook.com>
2025-01-21 15:58:46 +0500

    feat(sprintf): work with default char

--- src/include/s21_sprintf.h
+++ src/include/s21_sprintf.h
@@ -12,11 +12,16 @@
 #ifndef SRC_INCLUDE_S21_MEMSET_H_
 #define SRC_INCLUDE_S21_MEMSET_H_
 
+#include <limits.h>
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>   // before delete
+#include <stdlib.h>  // before delete
+#include <string.h>
 
 #include "include/common.h"
+#include "include/s21_atoi.h"
+#include "include/s21_strcat.h"
+#include "include/s21_strcspn.h"
 
 typedef struct {
   int width;
@@ -27,7 +32,16 @@ typedef struct {
 } format_t;
 
 int s21_sprintf(char *str, const char *format, ...);
-int check_digit(char c);
-void valid_width(const char *format, format_t *form);
+int check_digit(const char c);
+const char *value_width(const char *format, format_t *form, va_list arguments);
+int presence_point(const char c);
+const char *value_accuracy(const char *format, format_t *form,
+                           va_list arguments);
+int check_flags(const char c);
+const char *value_lenght(const char *format, format_t *form);
+const char *value_specifier(const char *format, format_t *form);
+char *type_definition(format_t *form, char *str, va_list arguments);
+char *format_char(format_t *form, char *str, va_list arguments);
+char *format_int(format_t *form, char *str, va_list arguments);
 
 #endif  // SRC_INCLUDE_S21_MEMSET_H_
--- src/modules/s21_sprintf.c
+++ src/modules/s21_sprintf.c
@@ -12,31 +12,40 @@
 #include "include/s21_sprintf.h"
 
 int s21_sprintf(char *str, const char *format, ...) {
-  format_t form = {0};
-  char *buffer = str;
-
   va_list arguments;
   va_start(arguments, format);
 
   while (*format) {
+    format_t form = {0};
+
     if (*format != '%') {
-      *buffer++ = *format++;
+      *str++ = *format++;
       continue;
     } else {
       ++format;
     }
 
-    if (check_digit(*format)) {
-      valid_width(format, &form);
+    if (check_flags(*format)) {
+      form.flag = *format;
+      ++format;
     }
 
-    printf("<%d>\n", form.width);
+    format = value_width(format, &form, arguments);
 
-    // if (presence_point(*format)) {
-    //   valid_accuracy(format, form);
-    // }
+    if (presence_point(*format)) {
+      ++format;
+      format = value_accuracy(format, &form, arguments);
+    }
 
-    // check_specidfier(*format, form);
+    format = value_lenght(format, &form);
+    format = value_specifier(format, &form);
+
+    str = type_definition(&form, str, arguments);
+    // printf("<%d>\n", form.width);
+    // printf("<%d>\n", form.accuracy);
+    // printf("<%c>\n", form.flag);
+    // printf("<%c>\n", form.lenght);
+    // printf("<%c>\n", form.spec);
   }
 
   va_end(arguments);
@@ -44,10 +53,150 @@ int s21_sprintf(char *str, const char *format, ...) {
   return 0;
 }
 
-int check_digit(char c) { return (c >= '0' && c <= '9'); }
+int check_digit(const char c) { return (c >= '0' && c <= '9'); }
 
-void valid_width(const char *format, format_t *form) {
-  while (check_digit(*format)) {
-    form->width = atoi(format);
+int presence_point(const char c) { return c == '.'; }
+
+int check_flags(const char c) {
+  char *flags = "-+#0 ";
+  char ch[2] = {c, '\0'};
+  return (s21_strcspn(ch, flags) == 0);
+}
+
+const char *value_width(const char *format, format_t *form, va_list arguments) {
+  if (*format != '*') {
+    form->width = s21_atoi(format);
+    while (check_digit(*format)) {
+      ++format;
+    }
+  } else {
+    form->width = va_arg(arguments, int);
+    ++format;
   }
+
+  return format;
+}
+
+const char *value_accuracy(const char *format, format_t *form,
+                           va_list arguments) {
+  if (*format != '*') {
+    form->accuracy = s21_atoi(format);
+    while (check_digit(*format)) {
+      ++format;
+    }
+  } else {
+    form->accuracy = va_arg(arguments, int);
+    ++format;
+  }
+
+  return format;
+}
+
+const char *value_lenght(const char *format, format_t *form) {
+  char *lenght = "Llh";
+  if (s21_strcspn(format, lenght) == 0) {
+    form->lenght = *format;
+    ++format;
+  }
+
+  return format;
+}
+
+const char *value_specifier(const char *format, format_t *form) {
+  char *specifiers = "cdeEfgGosuxXpni";
+
+  if (s21_strcspn(format, specifiers) == 0) {
+    form->spec = *format;
+    ++format;
+  }
+
+  return format;
+}
+
+char *type_definition(format_t *form, char *str, va_list arguments) {
+  switch (form->spec) {
+    case 'c':
+      str = format_char(form, str, arguments);
+      break;
+    case 'd':
+      str = format_int(form, str, arguments);
+      break;
+      // case 'e':
+      //   format_e(form, str);
+      //   break;
+      // case 'E':
+      //   format_E(form, str);
+      //   break;
+      // case 'f':
+      //   format_float(form, str);
+      //   break;
+      // case 'g':
+      //   format_g(form, str);
+      //   break;
+      // case 'G':
+      //   format_G(form, str);
+      //   break;
+      // case 'o':
+      //   format_o(form, str);
+      //   break;
+      // case 's':
+      //   format_string(form, str);
+      //   break;
+      // case 'u':
+      //   format_u(form, str);
+      //   break;
+      // case 'x':
+      //   format_x(form, str);
+      //   break;
+      // case 'X':
+      //   format_X(form, str);
+      //   break;
+      // case 'p':
+      //   format_p(form, str);
+      //   break;
+      // case 'n':
+      //   format_n(form, str);
+      //   break;
+      // case 'i':
+      //   format_i(form, str);
+      //   break;
+  }
+
+  return str;
+}
+
+char *format_char(format_t *form, char *str, va_list arguments) {
+  char c = va_arg(arguments, int);
+  int i = 1;
+
+  while (i < form->width) {
+    *str++ = ' ';
+    ++i;
+  }
+
+  if (form->lenght == 'l') {
+    char big_ch[100];
+
+    big_ch[0] = va_arg(arguments, wchar_t);
+    s21_strcat(str, big_ch);
+  } else {
+    *str = c;
+  }
+
+  ++str;
+
+  return str;
+}
+
+char *format_int(format_t *form, char *str, va_list arguments) {
+  int num = va_arg(arguments, int);
+
+  while (num) {
+    *str++ = (num % 10) + '0';
+    num = num / 10;
+  }
+
+  ++str;
+
+  return str;
 }

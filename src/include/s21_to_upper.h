e94aaeac033cd0706608304f9543abbe991c5933
pennyclo <alexandr_sav@outlook.com>
2025-02-19 20:07:21 +0500

    feat(to_upper): add s21_to_upper function

---
+++ src/include/s21_to_upper.h
@@ -0,0 +1,22 @@
+/**
+ * @file s21_to_upper.h
+ * @author Alexandr Afanasev (https://github.com/pennyclo)
+ * @brief Header file with function declaration s21_to_upper.
+ * @version 1.0
+ * @date 2025-02-14
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#ifndef SRC_INCLUDE_S21_TO_UPPER_H_
+#define SRC_INCLUDE_S21_TO_UPPER_H_
+
+#include <stdlib.h>
+
+#include "include/common.h"
+#include "include/s21_strlen.h"
+
+void *s21_to_upper(const char *str);
+
+#endif  // SRC_INCLUDE_S21_TO_UPPER_H_
---
+++ src/modules/s21_to_upper.c
@@ -0,0 +1,40 @@
+/**
+ * @file s21_to_upper.c
+ * @author Alexandr Afanasev (https://github.com/pennyclo)
+ * @brief
+ * @version 1.0
+ * @date 2025-02-14
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include "include/s21_to_upper.h"
+
+/**
+ * @brief Converts all characters in a string to uppercase.
+ *
+ * The function takes a string and returns a new string where all lowercase
+ * letters are converted to uppercase. Other characters remain unchanged. Memory
+ * for the new string is allocated dynamically and must be freed by the caller.
+ *
+ * @param str The input string to be converted.
+ * @return void* Pointer to the new string with uppercase characters.
+ */
+void *s21_to_upper(const char *str) {
+  char *string = (char *)calloc(s21_strlen(str) + 1, sizeof(char));
+  char *start = string;
+
+  while (*str) {
+    if (*str >= 'a' && *str <= 'z') {
+      *string = *str - 32;
+    } else {
+      *string = *str;
+    }
+
+    ++str;
+    ++string;
+  }
+
+  return start;
+}

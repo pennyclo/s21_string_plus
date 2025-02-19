a79e4c05696e5787d05572d0e0dbba49fc1cfdee
pennyclo <alexandr_sav@outlook.com>
2025-02-19 20:56:13 +0500

    feat(string): add main header

---
+++ src/s21_string.h
@@ -0,0 +1,39 @@
+/**
+ * @file s21_string.h
+ * @author Alexandr Afanasev (https://github.com/pennyclo)
+ * @brief Main header
+ * @version 1.0
+ * @date 2025-02-19
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#ifndef SRC_S21_STRING_H_
+#define SRC_S21_STRING_H_
+
+#include "include/s21_atoi.h"
+#include "include/s21_memchr.h"
+#include "include/s21_memcmp.h"
+#include "include/s21_memcpy.h"
+#include "include/s21_memmove.h"
+#include "include/s21_memset.h"
+#include "include/s21_sprintf.h"
+#include "include/s21_strcat.h"
+#include "include/s21_strchr.h"
+#include "include/s21_strcmp.h"
+#include "include/s21_strcpy.h"
+#include "include/s21_strcspn.h"
+#include "include/s21_strerror.h"
+#include "include/s21_strlen.h"
+#include "include/s21_strncat.h"
+#include "include/s21_strncmp.h"
+#include "include/s21_strncpy.h"
+#include "include/s21_strpbrk.h"
+#include "include/s21_strrchr.h"
+#include "include/s21_strspn.h"
+#include "include/s21_strstr.h"
+#include "include/s21_strtok.h"
+#include "include/s21_to_upper.h"
+
+#endif  // SRC_S21_STRING_H_
